367ae6c3a782a1075a00e7cbc6d8b56c61c6feae
Sachin Kumar <skraja012345678@gmail.com>
2025-08-05 08:30:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// console.log("Sachin")
+// let  boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+// boxes[2].style.backgroundColor = "red"
+
+// document.getElementById("redbox").style.backgroundColor = "pink"
+
+// document.querySelector(".box").style.backgroundColor = "green"
+
+console.log(document.querySelectorAll(".box"))
+
+document.querySelectorAll(".box").forEach(e => {
+    e.style.backgroundColor = "green";
+    
+});
+
+document.querySelector(".container").contains(e[1])
